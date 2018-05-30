b9434ef153013b389a4d54ea9ae131f9acba17bb
Greg Divine <greg4016@gmail.com>
2018-05-30 21:31:44 +0300

    messaging.requestPermission()

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import * as firebase from 'firebase';
 
 
 @Injectable()
@@ -8,6 +9,31 @@ export class UserService {
 
     set(userFromDatabase) {
         localStorage.setItem('user', JSON.stringify(userFromDatabase));
+
+        // const messaging = firebase.messaging();
+
+        // messaging.requestPermission()
+        //     .then(() => {
+        //         firebase.messaging().getToken()
+        //             .then(token => {
+        //                 console.log('Token received: ', token);
+
+        //                 messaging.onMessage(payload => {
+        //                     console.log(payload);
+        //                 });
+
+        //                 const updates = {};
+        //                 updates['/users/' + userFromDatabase.uid + '/messageToken'] = token;
+        //                 return firebase.database().ref().update(updates);
+        //             })
+        //             .catch(err => {
+        //                 console.log(err);
+        //             });
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //     });
+
         this.statusChange.emit(userFromDatabase);
     }
 
